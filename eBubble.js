3ccd6a85fa2b5ce0984e7cc2b80fb60002a4e51f
Khazise <71380558+Khazise@users.noreply.github.com>
2020-10-16 16:01:11 +0200

    Bubble Sort

--- eBubble.js
+++ eBubble.js
@@ -1,4 +1,4 @@
-function bulleSort(array = [])
+function bubbleSort(array = [])
 {
     let tmp=0;
     for(let y=0; y<array.length;y++)
@@ -17,4 +17,4 @@ function bulleSort(array = [])
     return array;
 }
 
-console.log(bulleSort([5,2,1,4,3]));
+console.log(bubbleSort([5,2,1,4,3]));
